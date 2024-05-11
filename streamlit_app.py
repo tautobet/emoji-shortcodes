98da40e32494dca61c3f93f11c05ccebb3729236
TrieuTruong <trieutruong.dev@gmail.com>
2024-05-11 16:01:56 +0700

    Initiate project

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,6 @@ from schedule import every, repeat, run_pending
 
 CODE_HOME           = os.path.abspath(os.path.dirname(__file__))
 
-# @st.cache(ttl=60*60*12, allow_output_mutation=True)
-# def fetch_emojis():
-#     resp = requests.get(
-#         'https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json')
-#     json = resp.json()
-#     codes, emojis = zip(*json.items())
-#     return pd.DataFrame({
-#         'Emojis': emojis,
-#         'Shortcodes': [f':{code}:' for code in codes],
-#     })
-
 
 @repeat(every(15).seconds)
 def strike_details():
