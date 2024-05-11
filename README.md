4faff6508e6c79175408062c8c4bab0dd5d31ef8
TrieuTruong <trieutruong.dev@gmail.com>
2024-05-11 15:09:42 +0700

    Initiate project

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
-# Streamlit emoji shortcodes
+# Streamlit app
 
-App showing all the emoji shortcodes supported by Streamlit
+Introduce streamlit app
 
-See https://share.streamlit.io/streamlit/emoji-shortcodes
+## Install Python, Poetry
+  
+1. Install Python 3.10.11 or above
+```
+pyenv install 3.10.11
+echo "3.10.11" >> .python-version
+```
+ 
+2. Install Poetry
+```
+pip install poetry
+```
+
+3. Configure project
+`copy .env-template .env`
+then install dependencies
+`poetry install`
+
+## Testing
+```
+poetry shell
+python -m pytest
+```
+
+
+
+## Run
+
+1. Run API server
+```
+ poetry shell
+ python -m babel.app
+```
+
+2. Run Streamlit app (stapp.py)
+```
+poetry shell
+python -m streamlit run horus/streamlit_app.py
+```
+   
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import requests
 import pandas as pd
+import time
+import horus.utils as utils
+
+from schedule import every, repeat, run_pending
 
 @st.cache(ttl=60*60*12, allow_output_mutation=True)
 def fetch_emojis():
@@ -12,6 +16,41 @@ def fetch_emojis():
         'Emojis': emojis,
         'Shortcodes': [f':{code}:' for code in codes],
     })
+
+
+@repeat(every(15).seconds)
+def strike_details():
+    # live_matches = Utils.sort_json(Utils.get_live_matches_1xbet(), "time_match")
+    live_matches = utils.sort_json(
+        utils.read_json_w_file_path(f'/Users/trieutruong/repo/autobet/src/report/matches.json'),
+        "time_match"
+    )
+    df = pd.DataFrame(
+        data=live_matches,
+        columns=("league", "team1", "team2", "team1_score", "team2_score", "time_match", "half", "scores")
+    )
+    st.table(df)
+
+
+if __name__ == "__main__":
+
+    # schedule.every(10).seconds.do(strike_details)
+
+    strike_details()
+    # schedule with params
+    # schedule.every(10).minutes.do(lambda: job('Hello ', 'world!'))
+
+    # # schedule.every(30).seconds.do(job)
+    # # schedule.every(10).minutes.do(job)
+    # # schedule.every().hour.do(job)
+    # # schedule.every().day.at("10:30").do(job)
+
+    while 1:
+        run_pending()
+        time.sleep(1)
+
+
+
 
 '''
 # Streamlit emoji shortcodes
@@ -25,6 +64,7 @@ Shortcodes are a way to enter emojis using pure ASCII. So you can type this `:sm
 *have to* use a shortcode)
 '''
 
-emojis = fetch_emojis()
+# emojis = fetch_emojis()
+#
+# st.table(emojis)
 
-st.table(emojis)
