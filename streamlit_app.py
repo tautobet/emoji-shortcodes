777bb7f7f659be45d2843c6ee6adba6f98cd14c5
TrieuTruong <trieutruong.dev@gmail.com>
2024-06-24 16:50:42 +0700

    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,75 +40,76 @@ with st.empty():
             url = f"{JSON_SERVER}/1x/"
             response = requests.request("GET", url, headers={}, data={})
 
-            # print(response.text)
-            data = response.json()
+            if response.status_code == 200:
+                # print(response.text)
+                data = response.json()
 
-            if data:
-                df = pd.DataFrame(
-                    data=data,
-                    columns=(
-                        "league",
-                        "team1",
-                        "team2",
-                        "score",
-                        "time_match",
-                        "add_time",
-                        "half",
-                        "prediction",
-                        "cur_prediction",
-                        "scores",
-                        "url")
-                ).sort_values(by='time_match', ascending=False)
-                st.dataframe(
-                    df,
-                    height=(len(data) + 1) * 35 + 3,
-                    column_config={
-                        "league": st.column_config.Column(
-                            label="League",
-                            width="medium"
-                        ),
-                        "team1": st.column_config.Column(
-                            label="T1",
-                            width="small"
-                        ),
-                        "team2": st.column_config.Column(
-                            label="T2",
-                            width="small"
-                        ),
-                        "score": st.column_config.TextColumn(
-                            label="Score",
-                            width="small"
-                        ),
-                        "time_match": st.column_config.Column(
-                            label="Time",
-                            width="small"
-                        ),
-                        "add_time": st.column_config.Column(
-                            label="ET",
-                            width="small"
-                        ),
-                        "half": st.column_config.Column(
-                            label="Half",
-                            width="small"
-                        ),
-                        "prediction": st.column_config.NumberColumn(
-                            label="Pre",
-                            format="%.1f".center(30),
-                            width="small"
-                        ),
-                        "cur_prediction": st.column_config.NumberColumn(
-                            label="Cur-Pre",
-                            format="%.1f".center(30),
-                            width="small"
-                        ),
-                        "url": st.column_config.LinkColumn(
-                            label="Link",
-                            display_text=f"Link",
-                            width="small"
-                        ),
+                if data:
+                    df = pd.DataFrame(
+                        data=data,
+                        columns=(
+                            "league",
+                            "team1",
+                            "team2",
+                            "score",
+                            "time_match",
+                            "add_time",
+                            "half",
+                            "prediction",
+                            "cur_prediction",
+                            "scores",
+                            "url")
+                    ).sort_values(by='time_match', ascending=False)
+                    st.dataframe(
+                        df,
+                        height=(len(data) + 1) * 35 + 3,
+                        column_config={
+                            "league": st.column_config.Column(
+                                label="League",
+                                width="medium"
+                            ),
+                            "team1": st.column_config.Column(
+                                label="T1",
+                                width="small"
+                            ),
+                            "team2": st.column_config.Column(
+                                label="T2",
+                                width="small"
+                            ),
+                            "score": st.column_config.TextColumn(
+                                label="Score",
+                                width="small"
+                            ),
+                            "time_match": st.column_config.Column(
+                                label="Time",
+                                width="small"
+                            ),
+                            "add_time": st.column_config.Column(
+                                label="ET",
+                                width="small"
+                            ),
+                            "half": st.column_config.Column(
+                                label="Half",
+                                width="small"
+                            ),
+                            "prediction": st.column_config.NumberColumn(
+                                label="Pre",
+                                format="%.1f".center(30),
+                                width="small"
+                            ),
+                            "cur_prediction": st.column_config.NumberColumn(
+                                label="Cur-Pre",
+                                format="%.1f".center(30),
+                                width="small"
+                            ),
+                            "url": st.column_config.LinkColumn(
+                                label="Link",
+                                display_text=f"Link",
+                                width="small"
+                            ),
 
-                    }
-                )
+                        }
+                    )
         except requests.exceptions.RequestException as e:
             logger.error(f'RequestException: {e}')
         except ConnectionResetError:
