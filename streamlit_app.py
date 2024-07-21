483a9921b8d3f91c1fb8c2683fb11d47a34906c2
TrieuTruong <trieutruong.dev@gmail.com>
2024-07-21 14:42:11 +0700

    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ with st.empty():
                             "cur_prediction",
                             "scores",
                             "url")
-                    ).sort_values(by='time_match', ascending=False)
+                    ).sort_values(by=['half', 'time_match'], ascending=[False, False], inplace=True)
                     st.dataframe(
                         df,
                         height=(len(data) + 1) * 35 + 3,
