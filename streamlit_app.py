58fa12ffd1c695187ae063dbf6f195de6b583ccb
TrieuTruong <trieutruong.dev@gmail.com>
2024-07-24 01:05:13 +0700

    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
+from horus.config import CODE_HOME
 
-st.page_link("app.py", label="LiveScores")
-st.page_link("pages/x1.py", label="1xBet")
-st.page_link("pages/x8.py", label="8xBet")
+st.page_link("streamlit_app.py", label="LiveScores")
+st.page_link(f"{CODE_HOME}/pages/x1.py", label="1xBet")
+st.page_link(f"{CODE_HOME}/pages/x8.py", label="8xBet")
