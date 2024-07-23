44cf6590a936ddaf583dd745bc5ada99be3e405c
TrieuTruong <trieutruong.dev@gmail.com>
2024-07-24 01:00:31 +0700

    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.page_link("app.py", label='LiveScores')
+st.page_link("app.py", label="LiveScores")
 st.page_link("pages/x1.py", label="1xBet")
 st.page_link("pages/x8.py", label="8xBet")
